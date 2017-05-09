e1628b492439cf94e27593f120ed7cf10565cab7
eaclimo <eaclimo@stu.naperville203.org>
2017-05-09 15:07:42 -0500

    day2

---
+++ HelloWorld/MotorProgram4.java
@@ -0,0 +1,14 @@
+
+
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.Motor;
+
+public class MotorProgram4
+{
+    public static void main(String[] args)
+    {
+        LCD.drawString( "Program 4", 0, 0 );
+        Button.waitForAnyPress();
+    }
+}
