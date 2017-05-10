430c3767c3b34384f847bc2820830bd6f6489cad
eaclimo <eaclimo@stu.naperville203.org>
2017-05-10 15:08:19 -0500

    day 3

---
+++ HelloWorld/SquareTracer.java
@@ -0,0 +1,29 @@
+
+import lejos.nxt.Motor;
+import lejos.robotics.navigation.DifferentialPilot;
+
+/**
+ *  uses a DifferentialPilot to trace out a square, using 
+ *      the travel and void rotate(double degrees) methods.
+ */
+
+public class SquareTracer
+{
+    DifferentialPilot pilot ;
+    
+    public void  drawSquare(float length)
+    {
+        for(int i = 0; i<4 ; i++)
+        {
+            pilot.travel(length);
+            pilot.rotate(90);                 
+        }
+    }
+    
+    public static void main(String[] args)
+    {
+        SquareTracer sq = new SquareTracer();
+        sq.pilot = new DifferentialPilot(2.25f, 5.5f, Motor.A, Motor.C);
+        sq.drawSquare(20);
+    }
+}
