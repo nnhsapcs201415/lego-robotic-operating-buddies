b3dd998e6c6f80da202707e69e591f720e14a2f7
eaclimo <eaclimo@stu.naperville203.org>
2017-05-22 15:06:30 -0500

    2

--- HelloWorld/MainClass.java
+++ HelloWorld/MainClass.java
@@ -4,25 +4,49 @@ import lejos.robotics.objectdetection.*;
 public class MainClass
 {
     private int state;
+
     public static void main (String[] args)
     {
-        System.out.println("Hello World");
+        System.out.println("Robot Challenge");
         Button.waitForAnyPress();
     }
-    
+
     public boolean run()
     {
-       
+
         while( true )
         {
             if( state == 0 ) // in box
-            {}
-            if( state == 1 ) // look for can
-            {}
-            if( state == 2 ) // push can 
-            {}
-            if( state == 3 ) // 
-            {}            
+            {
+                {
+                    boolean cond = false;
+
+                    ObjectDetect listener = new ObjectDetect();
+                    UltrasonicSensor us = new UltrasonicSensor(SensorPort.S4);
+                    RangeFeatureDetector fd = new RangeFeatureDetector(us, MAX_DETECT, 500);
+                    fd.addListener(listener);
+
+                    while (cond == false)
+                    {
+                        Motor.A.forward();
+                        Motor.C.backward();
+                        double range = feature.getRangeReading().getRange();
+                        if (range > 30)
+                        {
+                            
+                            
+
+                        }
+                    }
+
+                }
+                else if( state == 1 ) // look for can
+                {
+                }
+                else if( state == 2 ) // push can 
+                {}
+                else if( state == 3 ) // backup from line 
+                {}            
+            }
         }
     }
-}
