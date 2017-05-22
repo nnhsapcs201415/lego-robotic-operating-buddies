a5cc9d1f25e6e89908aec31ba889d822682530a8
eaclimo <eaclimo@stu.naperville203.org>
2017-05-22 14:35:14 -0500

    may 22

--- HelloWorld/MainClass.java
+++ HelloWorld/MainClass.java
@@ -1,11 +1,28 @@
 import lejos.nxt.*;
 import lejos.robotics.objectdetection.*;
 
-public class HelloWorld
+public class MainClass
 {
+    private int state;
     public static void main (String[] args)
     {
         System.out.println("Hello World");
         Button.waitForAnyPress();
+    }
+    
+    public boolean run()
+    {
+       
+        while( true )
+        {
+            if( state == 0 ) // in box
+            {}
+            if( state == 1 ) // look for can
+            {}
+            if( state == 2 ) // push can 
+            {}
+            if( state == 3 ) // 
+            {}            
+        }
     }
 }
