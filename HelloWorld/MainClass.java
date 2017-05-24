45ad1315dfc95508a704a0a708cffa457ba826e2
Jessique <gjliu@student.naperville203.org>
2017-05-24 10:15:59 -0500

    ho

--- HelloWorld/MainClass.java
+++ HelloWorld/MainClass.java
@@ -23,7 +23,7 @@ public class MainClass
             {}
             if( state == 3 ) // back up from line
             {
-                
+                Motor.C.backward();
             }            
         }
     }
